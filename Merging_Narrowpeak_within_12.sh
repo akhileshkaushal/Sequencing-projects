f4e8a5804ca3ce6d083ea750012c710e6f06778f
akhileshkaushal <akhilesh.kaushal@bcm.edu>
2018-04-24 09:57:08 -0500

    Update Merging_Narrowpeak_within_12.sh

--- Merging_Narrowpeak_within_12.sh
+++ Merging_Narrowpeak_within_12.sh
@@ -25,7 +25,9 @@ cd narrowPeak
 ls *.bed> list.beds
 split -l 1 list.beds list.beds.1way
 
-for g in list.beds.1waya?; do echo $g; for file in $(cat $g); do echo ${file}; e=$(echo ${file}| cut -f1 -d"."); echo $e;   bedtools merge -i ${file} -d 12500 > $e.merged.bed ; done & done
+for g in list.beds.1waya?; do echo $g; for file in $(cat $g); \
+do echo ${file}; e=$(echo ${file}| cut -f1 -d"."); echo $e; \
+bedtools merge -i ${file} -d 12500 > $e.merged.bed ; done & done
 
 cat *.bed > UNION_ALL.bed
 sort -k1,1 -k2,2n UNION_ALL.bed | bedtools merge -i stdin -d 0 > UNION_ALL_merged.bed
